da58bbd65817c650fd243734647dbd023346d891
Sangenis Pablo <sangenispablo@gmail.com>
2022-02-16 22:52:28 -0300

    Fixed loading

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,6 +1,6 @@
 export const GifItem = ({ id, title, url }) => {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__fadeIn">
       <img src={url} alt={title} className="card-img-top" />
       <div className="card-body">
         <p className="card-text">{title}</p>
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -3,13 +3,16 @@ import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = (query) => {
-
   const [state, setState] = useState({
     data: [],
     loading: true,
   });
 
   useEffect(() => {
+    setState({
+      data: [],
+      loading: true,
+    });
     getGifs(query).then((imgs) => {
       setState({
         data: imgs,
